78bc266ad5e0c1beef1be38ed2648933b911d0f1
oakamil <oakamil@gmail.com>
2026-01-03 18:09:08 -0800

    Include web content when building

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 cargo build --release
 
+rm -rf out
 rm  -rf dist/*
 mkdir -p out/cypress/bin
 mkdir -p dist
 cp -f target/release/cypress-display out/cypress/bin
+cp -rf web out/cypress/bin
 cp -f install.sh out
 cp -f cypress-display.service out
 pushd out
 zip -r ../dist/cypress-display.zip .
 popd
-rm -rf out
