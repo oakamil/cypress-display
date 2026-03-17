a8caeff9209e56500dc366fd6692383a51cf3650
oakamil <oakamil@gmail.com>
2026-03-16 22:35:17 -0700

    Move files to cypress-astro repo

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo build --release
-
-rm -rf out
-mkdir -p out/cypress/bin
-cp -f target/release/cypress-display out/cypress/bin
-cp -rf web out/cypress/bin
--- install.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-cp -rf cypress /home/cedar/
-sudo cp cypress-display.service /lib/systemd/system/
-sudo systemctl enable cypress-display
-sudo systemctl start cypress-display
